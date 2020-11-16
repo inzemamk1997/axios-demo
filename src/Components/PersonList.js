073e234b42a2bc049970478bd01f35e9b96e4451
Inzemam Khan <inzemamk1997@gmail.com>
2020-11-16 10:28:33 +0530

    axios-demo basic Completed

--- src/Components/PersonList.js
+++ src/Components/PersonList.js
@@ -1,18 +1,44 @@
 import React from "react";
 import axios from "axios";
+import ReactTable from "react-table-v6";
+import 'react-table-v6/react-table.css';
+
 export default class PersonList extends React.Component {
     state = {
-        persons: []
+        isButtonClicked: false,
+        persons: [],
+        columns: [{ Header: 'Name', accessor: 'name' }, { Header: 'Employee ID', accessor: 'id' }, { Header: "Employee Email", accessor: "email" }, { Header: "Employee Address", id: 'addressCity', accessor: obj => JSON.stringify(obj.address) }]
     }
-    componentDidMount() {
-        axios.get(`https://jsonplaceholder.typicode.com/users`)
-            .then(res => {
-                console.log(res);
-                this.setState({persons : res.data});
-            });
+    componentDidMount() { }
+
+    fetchData() {
+        axios.get(`https://jsonplaceholder.typicode.com/users`).then(res => {
+            this.setState({ persons: res.data });
+        });
     }
+
     render() {
-        return <ul>{this.state.persons.map(person => <li key={person.id}>{person.name}</li>)}</ul>;
- 
+        console.log(this.state.persons);
+
+        const fetchHandler = (event) => {
+            this.fetchData();
+            this.setState({ isButtonClicked: true });
+        };
+
+        return (
+            <>
+                {
+                    this.state.isButtonClicked ?
+                        <button onClick={fetchHandler}>Reload Data</button> :
+                        <button onClick={fetchHandler}>Fetch the data</button>
+                }
+                {
+                    this.state.isButtonClicked ?
+                        <ReactTable data={this.state.persons} columns={this.state.columns} /> :
+                        <></>
+                }
+            </>
+        );
+
     }
 }
